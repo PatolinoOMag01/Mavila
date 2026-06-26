8c03f97a4062af186017d83c9047ea7e5f89250a
PatolinoOMag01 <maatheuslim157@gmail.com>
2026-06-26 11:34:16 -0300

    retoques

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import { Routes, Route } from "react-router-dom";
-import { useEffect, useState } from "react";
 
 import Navbar from "./components/Navbar";
-import LoadingScreen from "./components/LoadingScreen";
 
 import Home from "./pages/Home";
 import Product from "./pages/Product";
@@ -13,37 +11,17 @@ import NotFound from "./pages/NotFound";
 import Profile from "./pages/Profile";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setLoading(false);
-    }, 2000);
-
-    return () => clearTimeout(timer);
-  }, []);
-
-  if (loading) {
-    return <LoadingScreen />;
-  }
-
   return (
     <>
       <Navbar />
 
       <Routes>
         <Route path="/" element={<Home />} />
-
         <Route path="/mv01" element={<Product />} />
-
         <Route path="/favorites" element={<Favorites />} />
-
         <Route path="/checkout" element={<Checkout />} />
-
         <Route path="/club" element={<MVClub />} />
-
         <Route path="/profile" element={<Profile />} />
-
         <Route path="*" element={<NotFound />} />
       </Routes>
     </>
